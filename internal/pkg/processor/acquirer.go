9e05225e8a5cf71dddd2b92c25cab4d85d75d8f9
Kim Prado <kimprado21@gmail.com>
2019-12-19 18:06:42 -0300

    Adicionar comportamento em Acquirer

--- internal/pkg/processor/acquirer.go
+++ internal/pkg/processor/acquirer.go
@@ -6,5 +6,11 @@ type AcquirerProcessor interface {
 	Process()
 }
 
+// AcquirerTransactionMapper representa comportamento capaz de fazer de-para,
+// e transformar dados para formato que adquirente espera.
+type AcquirerTransactionMapper interface {
+	MapTransaction()
+}
+
 // AcquirerID representa identificação de um Acquirer
 type AcquirerID string
