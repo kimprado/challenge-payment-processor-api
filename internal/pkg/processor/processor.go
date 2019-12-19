759d8252ab6120fb700828cb2024645e47b7d609
Kim Prado <kimprado21@gmail.com>
2019-12-19 01:02:27 -0300

    Criar tipos Authorization Request e Response

--- internal/pkg/processor/processor.go
+++ internal/pkg/processor/processor.go
@@ -3,5 +3,38 @@ package processor
 // Processor representa ponto de entrada para comportamento
 // da aplicação. O controlador do domínio.
 type Processor interface {
-	Process()
+	Process(t *TransactionDTO) (ar *AuthorizationResponse)
+}
+
+// Service implementa Processor é ponto de entrada
+// para domínio da aplicação.
+type Service struct {
+}
+
+// NewService cria instância de Sevice.
+func NewService() (s *Service) {
+	s = new(Service)
+	return
+}
+
+// Process delega processamento da transação para Acquirer.
+func (s *Service) Process(t *TransactionDTO) (ar *AuthorizationResponse) {
+	return
+}
+
+// AuthorizationRequest representa dados de transação.
+type AuthorizationRequest struct {
+	Transaction     *TransactionDTO
+	ResponseChannel *AuthorizationResponse
+}
+
+// AuthorizationResponse representa dados de transação.
+type AuthorizationResponse struct {
+	Authorized *AuthorizationMessage
+	Err        error
+}
+
+// AuthorizationMessage representa mensagem de sucesso na autorização
+type AuthorizationMessage struct {
+	Message string `json:"message"`
 }
