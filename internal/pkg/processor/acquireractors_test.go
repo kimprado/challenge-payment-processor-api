9e4096ae1aa087bc3900aecca8fac2a939408dc1
Kim Prado <kimprado21@gmail.com>
2019-12-19 14:26:25 -0300

    Criar teste unitário SendRequestToActor

---
+++ internal/pkg/processor/acquireractors_test.go
@@ -0,0 +1,51 @@
+// +build test unit
+
+package processor
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestSendRequestToActor(t *testing.T) {
+	t.Parallel()
+
+	actors := NewAcquirerActors()
+
+	testCases := []struct {
+		id          AcquirerID
+		request     *AuthorizationRequest
+		errExpected error
+	}{
+		{
+			"stone",
+			nil,
+			nil,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+
+			err := actors.Send(tc.id, tc.request)
+
+			if err != nil && tc.errExpected == nil {
+				t.Errorf("Erro inesperado %v", err)
+				return
+			}
+
+			got := errors.Is(err, tc.errExpected)
+
+			if got && tc.errExpected != nil {
+				return
+			}
+
+			if !got && tc.errExpected != nil {
+				t.Errorf("Esperado erro %v, mas obtido %v", tc.errExpected, err)
+				return
+			}
+
+		})
+	}
+
+}
