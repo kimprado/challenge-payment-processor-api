2bb2befc9e86ff603086542044bfef6ee3afde74
Kim Prado <kimprado21@gmail.com>
2019-12-19 00:02:30 -0300

    Mapear informações de uma transação

--- internal/pkg/processor/dto.go
+++ internal/pkg/processor/dto.go
@@ -1,15 +1,39 @@
 package processor
 
+import "time"
+
 // TransactionDTO representa dados de uma transação
 // para processamento.
 type TransactionDTO struct {
-	token string
-	total float64
-	itens []TransactionItemDTO
+	CardDTO
+	PurchaseDTO
+	MerchantDTO
 }
 
-// TransactionItemDTO representa itens de uma transação.
+// CardDTO representa dados do cartão.
+type CardDTO struct {
+	Token    string
+	Holder   string
+	Brand    string
+	Validity time.Time
+}
+
+// PurchaseDTO representa dados da compra.
+type PurchaseDTO struct {
+	Total        float64
+	Installments int
+	Items        []TransactionItemDTO
+}
+
+// TransactionItemDTO representa dados de uma transação.
 type TransactionItemDTO struct {
-	description  string
-	installments int
+	Description string
+	Price       float64
+}
+
+// MerchantDTO representa dados do vendedor.
+type MerchantDTO struct {
+	ID      string
+	Address string
+	Zipcode string
 }
