31c358f3315345ab62616369589a4fcbde03afea
Kim Prado <kimprado21@gmail.com>
2019-12-22 14:12:24 -0300

    Ajustar mapeamento de erros HTTP

--- internal/pkg/processor/api/error.go
+++ internal/pkg/processor/api/error.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/challenge/payment-processor/internal/pkg/processor"
@@ -11,18 +10,13 @@ import (
 
 func statusCode(e error) (s int) {
 
-	switch v := e.(type) {
+	switch e.(type) {
 	case *processor.CardNotFoundError:
 		s = http.StatusBadRequest // 404
 	case *errors.ParametersError:
 		s = http.StatusBadRequest // 400
 	default:
-		// TODO: Fazer com que este código não seja executado.
-		// Adotar alguma das seguintes medidas:
-		// 	- Aplicar testes/verificações.
-		// 	- Eventualmente substituir por log.err, caso não exista.
-		// 	- cobertura suficiente que garanta que não será executado.
-		panic(fmt.Sprintf("Tipo de erro não definido %T", v))
+		s = http.StatusInternalServerError // 500
 	}
 	return
 }
