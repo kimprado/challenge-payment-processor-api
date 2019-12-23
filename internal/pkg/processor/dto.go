af380843c3204a522c40259b5c061d13c0d95e79
Kim Prado <kimprado21@gmail.com>
2019-12-23 00:06:00 -0300

    Mapeamento JSON dos atributos

--- internal/pkg/processor/dto.go
+++ internal/pkg/processor/dto.go
@@ -6,7 +6,7 @@ import "time"
 // vinda do ambiente externo.
 type ExternalTransactionDTO struct {
 	*TransactionDTO
-	Token string
+	Token string `json:"token"`
 }
 
 // AcquirerTransactionDTO representa dados de uma transação
@@ -26,33 +26,33 @@ type TransactionDTO struct {
 
 // CardHiddenInfoDTO representa dados sensíveis do cartão.
 type CardHiddenInfoDTO struct {
-	Number string
-	CVV    string
+	Number string `json:"number"`
+	CVV    string `json:"cvv"`
 }
 
 // CardOpenInfoDTO representa abertos dados do cartão.
 type CardOpenInfoDTO struct {
-	Holder   string
-	Brand    string
-	Validity time.Time
+	Holder   string    `json:"holder"`
+	Brand    string    `json:"brand"`
+	Validity time.Time `json:"validity"`
 }
 
 // PurchaseDTO representa dados da compra.
 type PurchaseDTO struct {
-	Total        float64
-	Installments int
-	Items        []TransactionItemDTO
+	Total        float64              `json:"total"`
+	Installments int                  `json:"installments"`
+	Items        []TransactionItemDTO `json:"items"`
 }
 
 // TransactionItemDTO representa dados de itens de uma transação.
 type TransactionItemDTO struct {
-	Description string
-	Price       float64
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
 }
 
 // MerchantDTO representa dados do vendedor.
 type MerchantDTO struct {
-	ID      string
-	Address string
-	Zipcode string
+	ID      string `json:"id"`
+	Address string `json:"address"`
+	Zipcode string `json:"zipcode"`
 }
