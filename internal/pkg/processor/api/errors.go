29314fa62ef18b76fc116197c8c3efab17129b39
Kim Prado <kimprado21@gmail.com>
2019-12-23 18:57:46 -0300

    Organizar código

--- internal/pkg/processor/api/errors.go
+++ internal/pkg/processor/api/errors.go
@@ -13,12 +13,12 @@ func statusCode(e error) (s int) {
 	switch e.(type) {
 	case *processor.CardNotFoundError:
 		s = http.StatusNotFound // 404
-	case *errors.ParametersError:
-		s = http.StatusBadRequest // 400
 	case *processor.AcquirerProcessingError:
 		s = http.StatusServiceUnavailable // 503
 	case *processor.AcquirerConnectivityError:
 		s = http.StatusServiceUnavailable // 503
+	case *errors.ParametersError:
+		s = http.StatusBadRequest // 400
 	default:
 		s = http.StatusInternalServerError // 500
 	}
