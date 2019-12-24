4bedfa2a16acddf1b0d8ef743bb224d73ea3cabc
Kim Prado <kimprado21@gmail.com>
2019-12-24 06:30:43 -0300

    Remover código não usado

--- internal/pkg/processor/card.go
+++ internal/pkg/processor/card.go
@@ -5,10 +5,3 @@ type Card struct {
 	Number string `json:"number"`
 	CVV    string `json:"cvv"`
 }
-
-func newCard(number, cvv string) (c *Card) {
-	c = new(Card)
-	c.Number = number
-	c.CVV = cvv
-	return
-}
