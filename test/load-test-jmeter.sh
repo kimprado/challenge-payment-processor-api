babf148ac3be4c3271d7f1bff99b58641cc02fd7
Kim Prado <kimprado21@gmail.com>
2019-12-25 19:39:33 -0300

    Parametrizar caminho de CSV com casos de teste

--- test/load-test-jmeter.sh
+++ test/load-test-jmeter.sh
@@ -7,6 +7,7 @@ GUI_MODE=""
 MODE=""
 
 PATH_TEST_PLAN="./jmeter-load-test-plan.jmx"
+TEST_CASES_PATH="./jmeter-requests-config.csv"
 
 if [ -z "$1" ]
 then
@@ -23,9 +24,14 @@ if  [ ! -e $PATH_TEST_PLAN ]; then
     PATH_TEST_PLAN="./test/jmeter-load-test-plan.jmx"
 fi
 
+if  [ ! -e $TEST_CASES_PATH ]; then
+    TEST_CASES_PATH="./test/jmeter-requests-config.csv"
+fi
+
 export JMETER_API_HOST=localhost
 export JMETER_API_PORT=80
 export JMETER_API_CONTEXT=/api
+export JMETER_TEST_CASES_PATH=$TEST_CASES_PATH
 
 export HEAP="-Xms3g -Xmx4g -XX:MaxMetaspaceSize=3g -Xmn2g"
 
