fe6f4aaa7791656fd4724254251c2718ab554d7e
Kim Prado <kimprado21@gmail.com>
2019-12-25 20:20:32 -0300

    Renomear arquivo de configuração de testes

--- test/load-test-apachebench.sh
+++ test/load-test-apachebench.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Executando teste de carga"
 
-ab -n 61000 -c 103 -p POST_Transaction.json -T application/json -H 'X-ACQUIRER-ID: Stone' -m POST http://localhost:80/api/transactions
+ab -n 61000 -c 103 -p apachebench-POST_Transaction.json -T application/json -H 'X-ACQUIRER-ID: Stone' -m POST http://localhost:80/api/transactions
 
 # Verificar status do encerramento do teste, com "$?"
 if [ $? -eq 0 ]
