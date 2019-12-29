b0ec531f845a8cefcb2e3b5cec48bb2e10f4ca02
Kim Prado <kimprado21@gmail.com>
2019-12-29 01:19:50 -0300

    Corrigir texto e identação da home

--- internal/pkg/webserver/home.go
+++ internal/pkg/webserver/home.go
@@ -38,7 +38,7 @@ func (e Home) Serve(w http.ResponseWriter, r *http.Request, params httprouter.Pa
 				<a href='./version'>version</a> - Versão da aplicação(TAG). Informação preenchida na compilação ao gerar binário com 'make build' ou pelo CI.
 			</li>
 			<li>
-				<a href='./config'>config</a> - Configuração real usada em Runtime, não se restringe a, mas inicia com parametrização(Env/config-file)
+				<a href='./config'>config</a> - Configuração real usada em Runtime, não se restringe a, mas inicia com parametrização(Env/config-file).
 			</li>
 			<li>
 				<a href='./metrics'>metrics</a> - Méricas/Informações no formato Prometheus, como: Versões das dependências, tempo de resposta, total de requests, Tempo de GC.
@@ -50,9 +50,9 @@ func (e Home) Serve(w http.ResponseWriter, r *http.Request, params httprouter.Pa
 				<a href='http://localhost:9090/targets'>Prometheus</a> - Coleta métricas da aplicação e permite consultas sobre as mesmas.
 			</li>
 		</ul>
-		</body>
-		</html>
-		`
+	</body>
+	</html>
+	`
 
 	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	fmt.Fprintf(w, html)
